412df99716e307c852a956472c8afc5bc5bdd27d
BrainsEden131 <BrainsEden131@ANGLIA.LOCAL>
2019-06-28 20:47:39 +0100

    Added Unity project

---
+++ Assets/WordBarSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordBarSpawner : MonoBehaviour {
+
+    public GameObject go;
+    int NumberOfChars;
+
+    List<GameObject> gos;
+
+	// Use this for initialization
+	void Start () {
+        Instantiate(go, transform);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
