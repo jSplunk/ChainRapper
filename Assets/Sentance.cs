4e7bc82667de878ed60b11e84d6f4c8ee7954010
BrainsEden131 <BrainsEden131@ANGLIA.LOCAL>
2019-06-29 10:39:07 +0100

    Added words and blanks

---
+++ Assets/Sentance.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sentance : MonoBehaviour {
+
+    List<Blank> allBlanks;
+
+	// Use this for initialization
+	void Start () {
+        Blank[] tmp = GetComponentsInChildren<Blank>();
+
+        for(int i = 0; i < tmp.Length; i++)
+        {
+            allBlanks.Add(tmp[i]);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public bool isSentanceComplete()
+    {
+
+        int NumberOfBlanks = allBlanks.Capacity;
+        int BlanksFilled = 0;
+
+
+        foreach (Blank b in allBlanks)
+        {
+            if (b.isFilled)
+            {
+                BlanksFilled++;
+            }
+        }
+
+        if (BlanksFilled == NumberOfBlanks)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
