a94c6d445a7a32b5f4f58750fcc2847f21d6d3e1
TheRensei <>
2019-06-29 11:06:45 +0100

    Added return random word function + metronome begin

--- Assets/Scripts/WordContainer.cs
+++ Assets/Scripts/WordContainer.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public enum Type
 {
-    adjective,
-    noun,
+    NOUN,
+    VERB,
+    ADJECTIVE,
+    ADVERB
 }
 
 public class WordContainer : MonoBehaviour {
@@ -18,5 +20,29 @@ public class WordContainer : MonoBehaviour {
     }
 
     public Container[] wordCont;
+
+    //Redurn random work of Type
+    Container GetRandomWord(Type type)
+    {
+        Container temp = wordCont[Random.Range(0, wordCont.Length - 1)];
+        if (temp.type == type)
+        {
+            return temp;
+        }
+        else
+        {
+            GetRandomWord(type);
+        }
+
+        //It should never reach this point
+        Container empty = new Container();
+        return empty;
+    }
+
+    //Return any random word
+    Container GetRandomWord()
+    {
+        return wordCont[Random.Range(0, wordCont.Length - 1)];
+    }
     
 }
