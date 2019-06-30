a8d19fbd0137ef936d30031cd09b1fea0550ac78
jSplunk <joelxxzz@gmail.com>
2019-06-30 14:55:32 +0100

    Merged with fixtimer, end screen/gameover screen

---
+++ Assets/Scripts/Sentences.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sentences : MonoBehaviour {
+
+    public timerScript timerS;
+    Text sentenceText;
+    bool stringOutput = false;
+    public bool SentencesComplete = false;
+    float speedText = 2.0f;
+    //List<string> sentences;
+
+	// Use this for initialization
+	void Start () {
+        sentenceText = GetComponent<Text>();
+        sentenceText.text = "";
+        //sentences = wc.sentences;
+
+        Debug.Log(staticDataTrack.readOut.Count);
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //if (timerS.isGameOver && !stringOutput)
+        {
+
+            if (staticDataTrack.readOut.Count == 0) sentenceText.text = "None";
+
+            foreach (string s in staticDataTrack.readOut)
+            {
+                sentenceText.text += s;
+            }
+            stringOutput = true;
+        }
+
+        //if (timerS.isGameOver)
+        {
+            
+            sentenceText.transform.position = new Vector2(sentenceText.transform.position.x, sentenceText.transform.position.y+speedText);
+
+            if (sentenceText.transform.position.y - sentenceText.rectTransform.rect.height/2 > Screen.height)
+            {
+                SentencesComplete = true;
+            }
+        }
+
+
+    }
+}
--- Assets/Scripts/SentencesOLD.cs
+++ Assets/Scripts/SentencesOLD.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Sentences : MonoBehaviour {
+public class Sentencesold : MonoBehaviour {
 
     public WordCheck wc;
     Text sentenceText;
