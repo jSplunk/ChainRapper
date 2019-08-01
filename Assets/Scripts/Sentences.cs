45442dfaff9649d4854be2b40be65a183aa1f88c
joel <joelxxzz@gmail.com>
2019-08-01 17:04:36 +0200

    Fixed merge with build1.0.0

--- Assets/Scripts/Sentences.cs
+++ Assets/Scripts/Sentences.cs
@@ -9,12 +9,8 @@ public class Sentences : MonoBehaviour {
     Text sentenceText;
     bool stringOutput = false;
     public bool SentencesComplete = false;
-<<<<<<< HEAD
-    float speedText = 2.0f;
-=======
+
     float speedText = 50.0f;
->>>>>>> 6eaeef96de9b0cfc0e8523c23fe88dee4daf1f72
-    //List<string> sentences;
 
 	// Use this for initialization
 	void Start () {
@@ -28,12 +24,7 @@ public class Sentences : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-<<<<<<< HEAD
-        //if (timerS.isGameOver && !stringOutput)
-=======
         if (timerS.isGameOver && !stringOutput)
->>>>>>> 6eaeef96de9b0cfc0e8523c23fe88dee4daf1f72
         {
 
             if (staticDataTrack.readOut.Count == 0) sentenceText.text = "None";
@@ -45,17 +36,10 @@ public class Sentences : MonoBehaviour {
             stringOutput = true;
         }
 
-<<<<<<< HEAD
-        //if (timerS.isGameOver)
-        {
-            
-            sentenceText.transform.position = new Vector2(sentenceText.transform.position.x, sentenceText.transform.position.y+speedText);
-=======
         if (timerS.isGameOver)
         {
             
             sentenceText.transform.position = new Vector2(sentenceText.transform.position.x, sentenceText.transform.position.y+speedText * Time.deltaTime);
->>>>>>> 6eaeef96de9b0cfc0e8523c23fe88dee4daf1f72
 
             if (sentenceText.transform.position.y - sentenceText.rectTransform.rect.height/2 > Screen.height)
             {
